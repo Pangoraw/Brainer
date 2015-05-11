9c2a7a34ab56620f2d45ddae6166748f18feb2e1
Pangoraw <naydex.mc@gmail.com>
2015-05-11 22:32:29 +0200

    Compiled down this previous changes

--- public/js/app.js
+++ public/js/app.js
@@ -82,31 +82,137 @@ app.config([
 
 
 },{"./controllers/AppCtrl":5,"./controllers/CreateCtrl":6,"./controllers/EditCtrl":7,"./controllers/FileCtrl":8,"./controllers/FilesCtrl":9,"./controllers/UpdateCtrl":10,"./services/Files":11,"./services/Socket":12}],2:[function(require,module,exports){
-var FileList;
+var EventEmitter, FileList, FormCard,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
 
-module.exports = FileList = (function() {
-  FileList.prototype.goTo = null;
+EventEmitter = require('events');
 
-  FileList.prototype.name = null;
+FormCard = require('./FormCard');
 
-  FileList.prototype.id = null;
+module.exports = FileList = (function(superClass) {
+  extend(FileList, superClass);
 
-  FileList.prototype.type = null;
+  FileList.prototype.holder = null;
 
-  function FileList(name, id, type, goTo, parentId) {
-    this.name = name;
-    this.id = id;
-    this.type = type;
-    this.goTo = goTo;
+  FileList.prototype.files = [];
+
+  FileList.prototype.selectedNodes = [];
+
+  function FileList(holderId) {
+    this._onClick = bind(this._onClick, this);
+    var ref;
+    if ((ref = !holderId) != null ? ref : document.querySelector("ul#file-list")) {
+      return;
+    }
+    this.holder = document.querySelector("ul#file-list");
+    this.holder.addEventListener('click', this._onClick);
   }
+
+  FileList.prototype.getFileFromName = function(name) {
+    var file, i, len, ref;
+    ref = this.files;
+    for (i = 0, len = ref.length; i < len; i++) {
+      file = ref[i];
+      if (file.name === name) {
+        return file;
+      }
+    }
+    return -1;
+  };
+
+  FileList.prototype.append = function(file) {
+    var fileElt, pName, pType;
+    fileElt = document.createElement('li');
+    fileElt.classList.add('animated');
+    fileElt.classList.add('slideInLeft');
+    fileElt.classList.add(file.type);
+    pName = document.createElement('p');
+    pName.classList.add('name');
+    pName.innerHTML = file.name;
+    pType = document.createElement('p');
+    pType.classList.add('type');
+    pType.innerHTML = file.type;
+    fileElt.appendChild(pName);
+    fileElt.appendChild(pType);
+    this.holder.appendChild(fileElt);
+    return this.files.push(file);
+  };
+
+  FileList.prototype.clean = function() {
+    return this.holder.innerHTML = "";
+  };
+
+  FileList.prototype["delete"] = function() {
+    return this.emit('delete', this.getFileFromName(this.selectedNodes[0].children[0].innerHTML));
+  };
+
+  FileList.prototype.update = function() {
+    var oldName;
+    oldName = this.selectedNodes[0].children[0].innerHTML;
+    return new FormCard([
+      {
+        type: 'text',
+        label: 'New name'
+      }
+    ], (function(_this) {
+      return function(data) {
+        var file, newName;
+        newName = data[0];
+        file = _this.getFileFromName(_this.selectedNodes[0].children[0].innerHTML);
+        file.name = newName;
+        return _this.emit('update', file);
+      };
+    })(this));
+  };
+
+  FileList.prototype._onClick = function(event) {
+    var i, j, k, l, len, len1, len2, len3, node, ref, ref1, ref2, ref3, results, target;
+    event.preventDefault();
+    if (event.target.innerHTML.slice(0, 1) !== "<") {
+      target = event.target.parentElement;
+    } else {
+      target = event.target;
+    }
+    if (target === this.holder) {
+      ref = this.selectedNodes;
+      for (i = 0, len = ref.length; i < len; i++) {
+        node = ref[i];
+        node.classList.remove("selected");
+      }
+      ref1 = this.selectedNodes;
+      results = [];
+      for (j = 0, len1 = ref1.length; j < len1; j++) {
+        node = ref1[j];
+        results.push(this.selectedNodes.splice(node, 1));
+      }
+      return results;
+    } else if (this.selectedNodes.indexOf(target) === -1) {
+      ref2 = this.selectedNodes;
+      for (k = 0, len2 = ref2.length; k < len2; k++) {
+        node = ref2[k];
+        node.classList.remove("selected");
+      }
+      ref3 = this.selectedNodes;
+      for (l = 0, len3 = ref3.length; l < len3; l++) {
+        node = ref3[l];
+        this.selectedNodes.splice(node, 1);
+      }
+      this.selectedNodes.push(target);
+      return target.classList.add("selected");
+    } else {
+      return this.emit('activated', this.getFileFromName(target.children[0].innerHTML));
+    }
+  };
 
   return FileList;
 
-})();
+})(EventEmitter);
 
 
 
-},{}],3:[function(require,module,exports){
+},{"./FormCard":3,"events":13}],3:[function(require,module,exports){
 var FormCard,
   bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
@@ -150,8 +256,8 @@ module.exports = FormCard = (function() {
         for (j = 0, len1 = ref.length; j < len1; j++) {
           option = ref[j];
           optionElt = document.createElement('option');
-          optionElt.textContent = option;
-          optionElt.value = option;
+          optionElt.textContent = option.name;
+          optionElt.value = option.value;
           selectElt.appendChild(optionElt);
         }
         this.box.appendChild(selectElt);
@@ -395,31 +501,61 @@ FilesCtrl = function($scope, Socket, Files, $location) {
   $scope.files = [];
   $scope.q = '';
   Files.history.push("root");
+  FileList = new FileList("file-list");
+  FileList.on('update', (function(_this) {
+    return function(file) {
+      return Socket.emit('updateFile', file);
+    };
+  })(this));
+  FileList.on('delete', (function(_this) {
+    return function(file) {
+      Socket.emit('deleteFile', file);
+      return Socket.emit('getFiles', Files.getCurrentFolderId());
+    };
+  })(this));
+  FileList.on('activated', (function(_this) {
+    return function(file) {
+      if (file.type === "text") {
+        $location.path("/file/" + file._id).replace();
+        return $scope.$apply();
+      } else if (file.type === "folder") {
+        FileList.clean();
+        Socket.emit('getFiles', file._id);
+        Files.setCurrentFolderId(file._id);
+        Files.history.push(file._id);
+        return Files.updateCurrentFolder();
+      }
+    };
+  })(this));
   Files.setCurrentFolderId(Files.history[Files.history.length - 1]);
   Socket.emit('getFiles', Files.getCurrentFolderId());
   Socket.on('files', function(files) {
+    var file, i, len, ref, results;
     if (files != null) {
+      FileList.clean();
       if (!Array.isArray(files)) {
         files = [files];
       }
       $scope.files = files;
       if (files.length > 0) {
-        return Files.setCurrentFolderId(files[0].parent);
+        Files.setCurrentFolderId(files[0].parent);
       }
+      ref = $scope.files;
+      results = [];
+      for (i = 0, len = ref.length; i < len; i++) {
+        file = ref[i];
+        results.push(FileList.append({
+          _id: file._id,
+          name: file.name,
+          type: file.type
+        }));
+      }
+      return results;
     }
   });
   $scope.refresh = function() {
-    return $scope.request(Files.getCurrentFolderId(), 'folder');
-  };
-  $scope.request = function(id, type) {
-    if (type === "folder") {
-      Socket.emit('getFiles', id);
-      Files.setCurrentFolderId(id);
-      Files.history.push(id);
-      return Files.updateCurrentFolder();
-    } else if (type === "text") {
-      return $location.path("/file/" + id);
-    }
+    FileList.clean();
+    return Socket.emit('getFiles', Files.getCurrentFolderId());
   };
   $scope.goBackInHistory = function() {
     var id;
@@ -431,6 +567,9 @@ FilesCtrl = function($scope, Socket, Files, $location) {
     return $scope.request(id, 'folder');
   };
   $scope.goBackInTree = function() {
+    if (Files.getCurrentFolderId() !== "root") {
+      FileList.clean();
+    }
     return Socket.emit('getFilesInParent', Files.getCurrentFolderId());
   };
   $scope.goHome = function() {
@@ -443,7 +582,15 @@ FilesCtrl = function($scope, Socket, Files, $location) {
         label: "Name"
       }, {
         label: "Type",
-        options: ["text", "folder"],
+        options: [
+          {
+            value: "text",
+            name: "Text"
+          }, {
+            value: "folder",
+            name: "Folder"
+          }
+        ],
         type: "list"
       }
     ], _onCreate);
@@ -464,13 +611,11 @@ FilesCtrl = function($scope, Socket, Files, $location) {
       return $location.path("/home");
     };
   })(this);
-  return $scope.updateCurrentFolder = function() {
-    if (Files.getCurrentFolderId() === "root") {
-      alert('You can not update the root folder.');
-      return;
-    }
-    Files.updateCurrentFolder();
-    return $location.path("/updateFolder/" + (Files.getCurrentFolderId()));
+  $scope.changeName = function() {
+    return FileList.update();
+  };
+  return $scope.deleteFile = function() {
+    return FileList["delete"]();
   };
 };
 
@@ -557,5 +702,308 @@ Socket = function($rootScope) {
 module.exports = Socket;
 
 
+
+},{}],13:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+function EventEmitter() {
+  this._events = this._events || {};
+  this._maxListeners = this._maxListeners || undefined;
+}
+module.exports = EventEmitter;
+
+// Backwards-compat with node 0.10.x
+EventEmitter.EventEmitter = EventEmitter;
+
+EventEmitter.prototype._events = undefined;
+EventEmitter.prototype._maxListeners = undefined;
+
+// By default EventEmitters will print a warning if more than 10 listeners are
+// added to it. This is a useful default which helps finding memory leaks.
+EventEmitter.defaultMaxListeners = 10;
+
+// Obviously not all Emitters should be limited to 10. This function allows
+// that to be increased. Set to zero for unlimited.
+EventEmitter.prototype.setMaxListeners = function(n) {
+  if (!isNumber(n) || n < 0 || isNaN(n))
+    throw TypeError('n must be a positive number');
+  this._maxListeners = n;
+  return this;
+};
+
+EventEmitter.prototype.emit = function(type) {
+  var er, handler, len, args, i, listeners;
+
+  if (!this._events)
+    this._events = {};
+
+  // If there is no 'error' event listener then throw.
+  if (type === 'error') {
+    if (!this._events.error ||
+        (isObject(this._events.error) && !this._events.error.length)) {
+      er = arguments[1];
+      if (er instanceof Error) {
+        throw er; // Unhandled 'error' event
+      }
+      throw TypeError('Uncaught, unspecified "error" event.');
+    }
+  }
+
+  handler = this._events[type];
+
+  if (isUndefined(handler))
+    return false;
+
+  if (isFunction(handler)) {
+    switch (arguments.length) {
+      // fast cases
+      case 1:
+        handler.call(this);
+        break;
+      case 2:
+        handler.call(this, arguments[1]);
+        break;
+      case 3:
+        handler.call(this, arguments[1], arguments[2]);
+        break;
+      // slower
+      default:
+        len = arguments.length;
+        args = new Array(len - 1);
+        for (i = 1; i < len; i++)
+          args[i - 1] = arguments[i];
+        handler.apply(this, args);
+    }
+  } else if (isObject(handler)) {
+    len = arguments.length;
+    args = new Array(len - 1);
+    for (i = 1; i < len; i++)
+      args[i - 1] = arguments[i];
+
+    listeners = handler.slice();
+    len = listeners.length;
+    for (i = 0; i < len; i++)
+      listeners[i].apply(this, args);
+  }
+
+  return true;
+};
+
+EventEmitter.prototype.addListener = function(type, listener) {
+  var m;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events)
+    this._events = {};
+
+  // To avoid recursion in the case that type === "newListener"! Before
+  // adding it to the listeners, first emit "newListener".
+  if (this._events.newListener)
+    this.emit('newListener', type,
+              isFunction(listener.listener) ?
+              listener.listener : listener);
+
+  if (!this._events[type])
+    // Optimize the case of one listener. Don't need the extra array object.
+    this._events[type] = listener;
+  else if (isObject(this._events[type]))
+    // If we've already got an array, just append.
+    this._events[type].push(listener);
+  else
+    // Adding the second element, need to change to array.
+    this._events[type] = [this._events[type], listener];
+
+  // Check for listener leak
+  if (isObject(this._events[type]) && !this._events[type].warned) {
+    var m;
+    if (!isUndefined(this._maxListeners)) {
+      m = this._maxListeners;
+    } else {
+      m = EventEmitter.defaultMaxListeners;
+    }
+
+    if (m && m > 0 && this._events[type].length > m) {
+      this._events[type].warned = true;
+      console.error('(node) warning: possible EventEmitter memory ' +
+                    'leak detected. %d listeners added. ' +
+                    'Use emitter.setMaxListeners() to increase limit.',
+                    this._events[type].length);
+      if (typeof console.trace === 'function') {
+        // not supported in IE 10
+        console.trace();
+      }
+    }
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+EventEmitter.prototype.once = function(type, listener) {
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  var fired = false;
+
+  function g() {
+    this.removeListener(type, g);
+
+    if (!fired) {
+      fired = true;
+      listener.apply(this, arguments);
+    }
+  }
+
+  g.listener = listener;
+  this.on(type, g);
+
+  return this;
+};
+
+// emits a 'removeListener' event iff the listener was removed
+EventEmitter.prototype.removeListener = function(type, listener) {
+  var list, position, length, i;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events || !this._events[type])
+    return this;
+
+  list = this._events[type];
+  length = list.length;
+  position = -1;
+
+  if (list === listener ||
+      (isFunction(list.listener) && list.listener === listener)) {
+    delete this._events[type];
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+
+  } else if (isObject(list)) {
+    for (i = length; i-- > 0;) {
+      if (list[i] === listener ||
+          (list[i].listener && list[i].listener === listener)) {
+        position = i;
+        break;
+      }
+    }
+
+    if (position < 0)
+      return this;
+
+    if (list.length === 1) {
+      list.length = 0;
+      delete this._events[type];
+    } else {
+      list.splice(position, 1);
+    }
+
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.removeAllListeners = function(type) {
+  var key, listeners;
+
+  if (!this._events)
+    return this;
+
+  // not listening for removeListener, no need to emit
+  if (!this._events.removeListener) {
+    if (arguments.length === 0)
+      this._events = {};
+    else if (this._events[type])
+      delete this._events[type];
+    return this;
+  }
+
+  // emit removeListener for all listeners on all events
+  if (arguments.length === 0) {
+    for (key in this._events) {
+      if (key === 'removeListener') continue;
+      this.removeAllListeners(key);
+    }
+    this.removeAllListeners('removeListener');
+    this._events = {};
+    return this;
+  }
+
+  listeners = this._events[type];
+
+  if (isFunction(listeners)) {
+    this.removeListener(type, listeners);
+  } else {
+    // LIFO order
+    while (listeners.length)
+      this.removeListener(type, listeners[listeners.length - 1]);
+  }
+  delete this._events[type];
+
+  return this;
+};
+
+EventEmitter.prototype.listeners = function(type) {
+  var ret;
+  if (!this._events || !this._events[type])
+    ret = [];
+  else if (isFunction(this._events[type]))
+    ret = [this._events[type]];
+  else
+    ret = this._events[type].slice();
+  return ret;
+};
+
+EventEmitter.listenerCount = function(emitter, type) {
+  var ret;
+  if (!emitter._events || !emitter._events[type])
+    ret = 0;
+  else if (isFunction(emitter._events[type]))
+    ret = 1;
+  else
+    ret = emitter._events[type].length;
+  return ret;
+};
+
+function isFunction(arg) {
+  return typeof arg === 'function';
+}
+
+function isNumber(arg) {
+  return typeof arg === 'number';
+}
+
+function isObject(arg) {
+  return typeof arg === 'object' && arg !== null;
+}
+
+function isUndefined(arg) {
+  return arg === void 0;
+}
 
 },{}]},{},[1])
